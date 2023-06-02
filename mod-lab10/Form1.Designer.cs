1b1d878d18dc68fe3e20ca3fa4001a6b6ca39dc2
alinzz <97335671+alinzz@users.noreply.github.com>
2023-06-02 13:19:17 +0300

    new

---
+++ mod-lab10/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace mod_lab10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.x2 = new System.Windows.Forms.TextBox();
+            this.y2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Value = new System.Windows.Forms.TextBox();
+            this.ColSteps = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.y1 = new System.Windows.Forms.TextBox();
+            this.x1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // x2
+            // 
+            this.x2.Location = new System.Drawing.Point(87, 95);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(43, 22);
+            this.x2.TabIndex = 1;
+            // 
+            // y2
+            // 
+            this.y2.Location = new System.Drawing.Point(87, 123);
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(43, 22);
+            this.y2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(47, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "X2 =";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(46, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Y2 =";
+            // 
+            // Value
+            // 
+            this.Value.Location = new System.Drawing.Point(365, 53);
+            this.Value.Name = "Value";
+            this.Value.Size = new System.Drawing.Size(142, 22);
+            this.Value.TabIndex = 9;
+            this.Value.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Value_KeyPress);
+            // 
+            // ColSteps
+            // 
+            this.ColSteps.Location = new System.Drawing.Point(365, 81);
+            this.ColSteps.Name = "ColSteps";
+            this.ColSteps.Size = new System.Drawing.Size(142, 22);
+            this.ColSteps.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(263, 40);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // y1
+            // 
+            this.y1.Location = new System.Drawing.Point(87, 67);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(43, 22);
+            this.y1.TabIndex = 12;
+            // 
+            // x1
+            // 
+            this.x1.Location = new System.Drawing.Point(87, 40);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(43, 22);
+            this.x1.TabIndex = 13;
+            this.x1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.x1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(46, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 27);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "X1 = ";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(47, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 26);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Y1 = ";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(229, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 25);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Точность";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(180, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(179, 25);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Количество членов ряда";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] { "Использование угла", "Уравнение прямой" });
+            this.comboBox1.Location = new System.Drawing.Point(269, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(194, 24);
+            this.comboBox1.TabIndex = 18;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 707);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.x1);
+            this.Controls.Add(this.y1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ColSteps);
+            this.Controls.Add(this.Value);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.y2);
+            this.Controls.Add(this.x2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.ComboBox comboBox1;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox y1;
+        private System.Windows.Forms.TextBox x1;
+
+        private System.Windows.Forms.TextBox Value;
+        private System.Windows.Forms.TextBox ColSteps;
+
+        private System.Windows.Forms.TextBox x2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.TextBox y2;
+
+        #endregion
+    }
+}
---
+++ mod-lab10/Form1.cs
@@ -0,0 +1,205 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+ namespace mod_lab10
+ {
+     public partial class Form1 : Form
+     {
+         public Form1()
+         {
+             InitializeComponent();
+             Value.Enabled = false;
+             ColSteps.Enabled = false;
+         }
+
+         public void WithAngle(Point startPoint, Point endPoint, float accuracy, int numberOfMembers)
+         {
+             double x = startPoint.X, y = startPoint.Y;
+             int step = 1;
+             Pen pen = new Pen(Color.Black, 2);
+             Pen pen2 = new Pen(Color.Green, 5);
+             Pen pen3 = new Pen(Color.Red, 5);
+             double angle = Atan(Math.Abs(endPoint.Y - startPoint.Y) /
+                                 (double)Math.Abs(endPoint.X - startPoint.X), numberOfMembers) * 180 / Math.PI;
+             double length = Math.Sqrt(Math.Pow(endPoint.Y - startPoint.Y, 2) + Math.Pow(endPoint.X - startPoint.X, 2));
+             double sin = Sin(angle, numberOfMembers);
+             double cos = Cos(angle, numberOfMembers);
+             Graphics graphics = changeGraphics();
+             graphics.DrawEllipse(new Pen(Color.Blue, 5), endPoint.X - accuracy, endPoint.Y - accuracy, accuracy * 2,
+                 accuracy * 2);
+             graphics.DrawEllipse(new Pen(Color.Green, 5), startPoint.X - 5, startPoint.Y - 5, 10, 10);
+             int stepForX = (endPoint.X > startPoint.X) ? 1 : -1;
+             int stepForY = (endPoint.Y > startPoint.Y) ? 1 : -1;
+             while (length > accuracy)
+             {
+                 x += stepForX * (step * cos);
+                 y += stepForY * (step * sin);
+                 graphics.DrawEllipse(new Pen(Color.Red, 5), (int)x, (int)y, 1, 1);
+                 double previousLength = length;
+                 length = Math.Sqrt(Math.Pow(endPoint.Y - y, 2) + Math.Pow(endPoint.X - x, 2));
+                 if (previousLength < length)
+                 {
+                     MessageBox.Show("Промах не в норме");
+                     return;
+                 }
+             }
+
+             MessageBox.Show("Промах в норме");
+         }
+
+         private void LineEquation(Point startPoint, Point endPoint)
+         {
+             if (endPoint.X - startPoint.X == 0)
+             {
+                 MessageBox.Show("k = бесконечности");
+                 return;
+             }
+
+             double k = ((double)(endPoint.Y - startPoint.Y)) / (endPoint.X - startPoint.X);
+             double b = startPoint.Y - k * startPoint.X;
+             Graphics graphics = changeGraphics();
+             graphics.DrawEllipse(new Pen(Color.Blue, 5), endPoint.X - 5, endPoint.Y - 5, 10, 10);
+             graphics.DrawEllipse(new Pen(Color.Green, 5), startPoint.X - 5, startPoint.Y - 5, 10, 10);
+             int step = (endPoint.X > startPoint.X) ? 1 : -1;
+             int currentX = startPoint.X;
+             while (currentX != endPoint.X)
+             {
+                 Thread.Sleep(20);
+                 graphics.DrawEllipse(new Pen(Color.Red, 5), new RectangleF(currentX, (float)(k * currentX + b), 1, 1));
+                 currentX += step;
+             }
+         }
+
+         double Atan(double tangent, int numberOfMembers)
+         {
+             double atan = 0;
+             if (-1 <= tangent && tangent <= 1)
+             {
+                 for (int i = 0; i < numberOfMembers; i++)
+                     atan += Math.Pow(-1, i) * Math.Pow(tangent, 2 * i + 1) / (2 * i + 1);
+             }
+             else
+             {
+                 int coef = (tangent >= 1) ? 1 : -1;
+                 atan = coef * Math.PI / 2;
+                 for (int i = 0; i < numberOfMembers; i++)
+                 {
+                     atan -= Math.Pow(-1, i) / ((2 * i + 1) * Math.Pow(tangent, 2 * i + 1));
+                 }
+             }
+
+             return atan;
+         }
+
+         int Fact(int n)
+         {
+             if (n == 0) return 1;
+             return n * Fact(n - 1);
+         }
+
+         private double Sin(double angle, int numberOfMembers)
+         {
+             double sin = 0;
+             for (int i = 0; i < numberOfMembers; i++)
+             {
+                 int factorial = Fact(2 * i + 1);
+                 sin += Math.Pow(-1, i) * (Math.Pow(angle * Math.PI / 180, 2 * i + 1) / factorial);
+             }
+
+             return sin;
+         }
+
+         private double Cos(double angle, int colsteps)
+         {
+             double cos = 0;
+             for (int i = 0; i < colsteps; i++)
+             {
+                 int factorial = Fact(2 * i);
+                 cos += Math.Pow(-1, i) * Math.Pow(angle * Math.PI / 180, 2 * i) / factorial;
+             }
+
+             return cos;
+         }
+         
+
+         private Graphics changeGraphics()
+         {
+             Graphics graphics = CreateGraphics();
+             graphics.TranslateTransform(Width / 2, Height / 2);
+             graphics.ScaleTransform(0.5f, 0.5f);
+             return graphics;
+         }
+
+         private void Form1_Paint(object sender, PaintEventArgs e)
+         {
+             Pen blackPen = new Pen(Color.Black, 1);
+             Graphics graphics = changeGraphics();
+             graphics.DrawLine(blackPen, new Point(-1000, 0), new Point(1000, 0));
+             graphics.DrawLine(blackPen, new Point(0, 1000), new Point(0, -1000));
+         }
+
+
+         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+         {
+             if (comboBox1.SelectedIndex == 0)
+             {
+                 Value.Enabled = true;
+                 ColSteps.Enabled = true;
+             } else {
+                 Value.Enabled = false;
+                 ColSteps.Enabled = false;
+             }
+         }
+
+         private void x1_KeyPress(object sender, KeyPressEventArgs e)
+         {
+             char number = e.KeyChar;
+             if (!Char.IsDigit(number) && number != 8 && number != '-')
+                 e.Handled = true;
+         }
+
+         private void Value_KeyPress(object sender, KeyPressEventArgs e)
+         {
+             char number = e.KeyChar;
+             if (!Char.IsDigit(number) && number != 8 && number != ',')
+                 e.Handled = true;
+         }
+
+         private void button1_Click(object sender, EventArgs e)
+         {
+             if (x1.Text == "" || y1.Text == "" || x2.Text == "" || y2.Text == ""
+                 || (comboBox1.SelectedIndex == 0 && (Value.Text == "" || ColSteps.Text == "")))
+             {
+                 MessageBox.Show("Неправильно введены данные");
+                 return;
+             }
+             Point startingPoint = new Point(int.Parse(x1.Text), int.Parse(y1.Text));
+             Point endPoint = new Point(int.Parse(x2.Text), int.Parse(y2.Text));
+             switch (comboBox1.SelectedIndex)
+             {
+                 case 0:
+                 {
+                     float accuracy = float.Parse(Value.Text);
+                     int numberOfMembers = int.Parse(ColSteps.Text);
+                     WithAngle(startingPoint, endPoint, accuracy, numberOfMembers);
+                     break;
+                 }
+                 case 1:
+                 {
+                     LineEquation(startingPoint, endPoint);
+                     break;
+                 }
+             }
+         }
+         
+     }
+ }
